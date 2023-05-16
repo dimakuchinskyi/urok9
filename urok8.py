467d6d6e9d9f7f45618f20d52063cd0919d2a8f5
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-16 20:31:36 +0300

    Завдання два

--- urok8.py
+++ urok8.py
@@ -1,6 +1,6 @@
 #Практична робота
 #1
-
+'''
 class InvalidUsernameError(Exception):
     def __init__(self, username):
         self.username = username
@@ -20,3 +20,23 @@ try:
 except InvalidUsernameError as e:
     print(f'Неправильне ім`я користувача {e.username}'
           f'Мінімум 5 символів.')
+'''
+#2
+class InvalidPasswordError(Exception):
+    def __init__(self, password):
+        self.password = password
+        super().__init__("Invalid password")
+
+def validate_password(password):
+    # Перевірка на умови пароля
+    if len(password) < 8 or not any(char.isdigit() for char in password):
+        raise InvalidPasswordError(password)
+
+# Приклад використання
+try:
+    password = input("Введіть пароль: ")
+    validate_password(password)
+    print("Пароль валідний!")
+except InvalidPasswordError as e:
+    print(f"Помилка перевірки пароля: {e.password} є неправильним.")
+#3
