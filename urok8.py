4b00c227d435090f973eceedc96553b4024d4cb1
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-16 19:46:25 +0300

    Клас Exaption

--- urok8.py
+++ urok8.py
@@ -7,3 +7,22 @@ try:
 except:
     print('no problems')
 print('any code...')
+
+def checker(var):
+    if type (var) != str:
+        raise TypeError(f'Ми не працюємо з {type(var)}, нам треба str')
+    else:
+        return var
+a = '1234'
+checker(a)
+
+class BuildingHouseError(Exception):
+    def __str__(self):
+        return 'Щось не те, дуже багато і дорого'
+def check_matreial_build(amount, limit):
+    if amount > limit:
+        return 'Достатньо матеріалів!'
+    else:
+        BuildingHouseError()
+print(check_material_build(10, 300))
+
