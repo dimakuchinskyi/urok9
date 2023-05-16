253db7c498bbe6936b74fe303682a5430ddedfec
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-16 20:08:22 +0300

    Завдання один

--- urok8.py
+++ urok8.py
@@ -1,28 +1,22 @@
-#Клас урок
+#Практична робота
 #1
+
+class InvalidUsernameError(Exception):
+    def __init__(self, username):
+        self.username = username
+#InvalidLenghtError
+#InvalidCharacterError
+#
+
+def register_user(username):
+    if len(username) < 5:
+        raise InvalidUsernameError(username)
+    else:
+        print('Вас зареєстровано!')
+#Приклад використання
 try:
-    print('start code')
-    print(error)
-    print('end')
-except:
-    print('no problems')
-print('any code...')
-
-def checker(var):
-    if type (var) != str:
-        raise TypeError(f'Ми не працюємо з {type(var)}, нам треба str')
-    else:
-        return var
-a = '1234'
-checker(a)
-
-class BuildingHouseError(Exception):
-    def __str__(self):
-        return 'Щось не те, дуже багато і дорого'
-def check_matreial_build(amount, limit):
-    if amount > limit:
-        return 'Достатньо матеріалів!'
-    else:
-        BuildingHouseError()
-print(check_material_build(10, 300))
-
+    username = input('Введіть ім`я користувача ')
+    register_user(username)
+except InvalidUsernameError as e:
+    print(f'Неправильне ім`я користувача {e.username}'
+          f'Мінімум 5 символів.')
